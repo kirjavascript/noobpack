7e5d4dd4470aec9dc02eb65f950dc8a96a976f8d
snkenjoi <snkenjoi@gmail.com>
2016-05-24 23:56:43 +0100

    d3 example

--- modules/js/index.js
+++ modules/js/index.js
@@ -1,6 +1,14 @@
 import '../scss/index.scss'
 import flip from '../html/flip.html';
+import * as d3 from './util/d3';
 
-document.write(flip({
+d3.select('body').html(flip({
     info: '(╯°□°）╯︵ ┻━┻'
 }))
+
+let rainbow = d3.scaleRainbow();
+
+d3.interval(function(elapsed) {
+    d3.select('.flip')
+        .style('color', rainbow(elapsed))
+}, 200);
--- modules/js/util/d3.js
+++ modules/js/util/d3.js
@@ -1,8 +1,4 @@
 export {
-  version
-} from "./build/package";
-
-export {
   bisect,
   bisectRight,
   bisectLeft,
